f4ebc4f346fa499a6ffc49a45563a413576868f5
teosims5 <teo.simeonov@code.berlin>
2022-06-15 18:21:08 +0200

    more app work

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask
+from crypt import methods
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+cookies_data = {
+    'chocolate-chip' : {'name' : 'Chocolate Chip', 'price' : '€1.50'},
+    'oatmeal-raisin' : {'name' : 'Oatmeal Raisin', 'price' : '€1.00'},
+    'sugar' : { 'name' : 'Sugar', 'price' : '€0,75'},
+    'peanut-butter' : {'name' : 'Peanut Butter', 'price' : '€0,50'},
+    'oatmeal' : {'name' : 'Oatmeal', 'price' : '€0,25'},
+    'salted-caramel' : {'name' : 'Salted Caramel', 'price' : '€1,00'}
+}
 
-app.run()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return '''<h1>Hello World!</h1>
+    <a href="/about">About Page</a>'''
+
+@app.route('/about')
+def about():
+    return 'I like cookies'
+
+@app.route('/about-me')
+def about_me():
+    return redirect(url_for('/about'))
+
+@app.route('/cookies/<slug>')
+def cookie(slug):
+    return slug
 
 def apprun ():
     if __name__ == '__main__':
